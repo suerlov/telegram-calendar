a1468aee4fe3d4f0646b709117b5d4a2a4ae858e
tantian <934171065@qq.com>
2024-03-11 20:07:50 +0700

    Update messageSender.js

--- messageSender.js
+++ messageSender.js
@@ -16,8 +16,8 @@ md`Hello!
 
 ${md.bold("Group Meetup Facilitator")} helps you organize group meetups, e. g. in-person events or\
  calls. Here's how it works:
- 
-1. Organizer accesses ${md.link("the calendar", `https://t.me/${this.botName}/calendar`)} \
+
+1. Organizer accesses ${md.link("the calendar", `https://t.me/${this.botName}/my93Tantia`)} \
 to set options for when the group can meet
 2. Organizer receives a link to share with the group
 3. Group members vote for the options that work for them
@@ -25,7 +25,7 @@ to set options for when the group can meet
 
 And that's it!
 
-Go to ${md.link("the calendar", `https://t.me/${this.botName}/calendar`)} to get started`;
+Go to ${md.link("the calendar", `https://t.me/${this.botName}/my93Tantia`)} to get started`;
 
         return await this.sendMessage(chatId, md.build(message), replyToMessageId);
     }
@@ -40,12 +40,12 @@ or just copy the link from it.`;
         await this.sendMessage(chatId, md.build(message));
 
         const linkMessage =
-md`${userName} uses ${md.bold("Group Meetup Facilitator")} to organize a group meetup! 
+md`${userName} uses ${md.bold("Group Meetup Facilitator")} to organize a group meetup!
 
 Please click on the link below to vote for the dates that work for you. You can vote for multiple dates:
 
 ${md.link(`https://t.me/${this.botName}/calendar?startapp=${calendarRef}`, `https://t.me/${this.botName}/calendar?startapp=${calendarRef}`)}`;
-    
+
         return await this.sendMessage(chatId, md.build(linkMessage));
     }
 }
